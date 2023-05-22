d843252098ccb78074493c2dc3104039f9b9d93d
shabit24 <hossain.shabit24@gmail.com>
2023-05-22 15:29:26 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilia;
+	
+public:
+	mahasiswa(int pId, string pNama, float pNilia) :id(pId, nama(pNama), nilia(pNilia){
+
+	}
+	~mhasiswa() {
+		cout << "Id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilia = " << nilia << endl;
+	}
+};
+
+int main() {
+	mahasiswa mhs(12, "Lia, 90.5");
+	return 0;
+}
