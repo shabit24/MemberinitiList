abf38e65284e61e2fb4bb773875397e312e68e92
shabit24 <hossain.shabit24@gmail.com>
2023-05-22 21:02:54 +0700

    Jantung file

---
+++ mainJM/jantung.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+
+	}
+	~jantung() {
+		"jantung dimatikan\n";
+	}
+};
+#endif
